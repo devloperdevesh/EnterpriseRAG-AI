be8d01383e15579117f5b45777ddb2fbb79a29b2
Devesh Chauhan <deveshchauhandk_cse23@its.edu.in>
2026-02-02 14:43:43 +0530

    updated user.py and base.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, Integer, String
-from app.db.session import Base
+from app.db.base import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    tenant_id = Column(String, nullable=False)
+
+    tenant_id = Column(String, index=True, nullable=False)
     role = Column(String, nullable=False)
